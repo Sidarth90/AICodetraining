c001dfd689e9c556126b095b802fe7d16ee98e17
Sidarth90 <sidarth.radjou@gmail.com>
2025-07-30 12:49:26 +0200

    Move Image Generator project to root directory

--- projects/AI_image_generator/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import ImageGenerator from "../components/ImageGenerator";
-
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center p-8">
-      <ImageGenerator />
-    </main>
-  );
-}
